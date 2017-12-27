38269dc5cc9114983ba82c6e0dbe96e03ac0f88a
Jeongnny <kjeun421@gmail.com>
2017-12-27 17:34:56 +0900

    lab4-2 Q1

---
+++ lab4_2/lab4_2_Q1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+int main(void){
+	int i;
+	struct timeval start_t, end_t;
+	gettimeofday(&start_t, NULL);
+	
+	for(i = 0; i < 9999999; i++)
+		rand();
+	
+	gettimeofday(&end_t, NULL);
+
+	printf("[start]      %f\n", start_t.tv_sec + start_t.tv_usec*0.000001);
+	printf("[end]        %f\n", end_t.tv_sec + end_t.tv_usec*0.000001);
+	printf("[difference] %f\n", (end_t.tv_sec + end_t.tv_usec*0.000001)-(start_t.tv_sec + start_t.tv_usec*0.000001));
+	
+	return 0;
+}
+
+
